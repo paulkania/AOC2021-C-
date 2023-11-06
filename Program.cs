2c8a1e0e5c406057893fdcfc97cc28cf768b42ff
paulkania <p.rozehnal@gmail.com>
2023-11-06 14:52:17 -0500

    Add files via upload
    HOW TO START A C# PROJECT
    terminal: go to project folder
    cmd: code .
    shift+command+p: -> .NET new project -> console app.
    ez

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿var input = File.ReadAllLines(@"./1r.txt")
+    .Select(int.Parse)
+    .ToList();
+
+
+foreach(var i in input)
+{
+     Console.WriteLine(i);
+}
+
+static int CalculateDepthIncreases(List<int> input)
+{
+    int NumberofTimesDepthIncreases =0;
+    int i = 1;
+
+    while (i < input.Count())
+    {
+        if (input[i] > input[i-1])
+        {
+            NumberofTimesDepthIncreases++;
+        }
+        i++;
+    }
+    return NumberofTimesDepthIncreases;
+}
+
+var partone = CalculateDepthIncreases(input);
+Console.WriteLine(partone);
